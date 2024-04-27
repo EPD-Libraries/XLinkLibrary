1b6c9f0a9970143a20d278fcd32d5c411cb2f856
ArchLeaders <archleadership28@gmail.com>
2024-04-27 11:39:23 -0700

    Create Header

---
+++ src/XLinkLibrary/Structures/XLinkHeader.cs
@@ -0,0 +1,25 @@
+﻿namespace XLinkLibrary.Structures;
+
+public struct XLinkHeader
+{
+    public const uint MAGIC = 0x4B4E4C58;
+
+    public uint Magic;
+    public uint DataSize;
+    public uint Version;
+    public uint NumResParam;
+    public uint NumResAssetParam;
+    public uint NumResTriggerOverwriteParam;
+    public Pointer TriggerOverwriteParamTablePos;
+    public Pointer LocalPropertyNameRefTablePos;
+    public uint NumLocalPropertyNameRefTable;
+    public uint NumLocalPropertyEnumNameRefTable;
+    public uint NumDirectValueTable;
+    public uint NumRandomTable;
+    public uint NumCurveTable;
+    public uint NumCurvePointTable;
+    public Pointer ExRegionPos;
+    public int UserCount;
+    public Pointer ConditionTablePos;
+    public Pointer NameTablePos;
+}
