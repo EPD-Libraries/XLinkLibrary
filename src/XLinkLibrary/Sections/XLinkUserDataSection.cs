aa5a5b055fe92fd44c3f2c04c8d5af5169797d6d
ArchLeaders <archleadership28@gmail.com>
2024-04-27 11:40:34 -0700

    Parse UserData section

---
+++ src/XLinkLibrary/Sections/XLinkUserDataSection.cs
@@ -0,0 +1,15 @@
+﻿using Revrs;
+
+namespace XLinkLibrary.Sections;
+
+public ref struct XLinkUserDataSection
+{
+    public Span<uint> Hashes;
+    public Span<uint> Offsets;
+
+    public XLinkUserDataSection(ref RevrsReader reader, int userCount)
+    {
+        Hashes = reader.ReadSpan<uint>(userCount);
+        Offsets = reader.ReadSpan<uint>(userCount);
+    }
+}
---
+++ src/XLinkLibrary/XLinkView.cs
@@ -0,0 +1,17 @@
+﻿using Revrs;
+using XLinkLibrary.Sections;
+using XLinkLibrary.Structures;
+
+namespace XLinkLibrary;
+
+public ref struct XLinkView
+{
+    public XLinkHeader Header;
+    public XLinkUserDataSection UserData;
+
+    public XLinkView(ref RevrsReader reader)
+    {
+        Header = reader.Read<XLinkHeader>();
+        UserData = new(ref reader, Header.UserCount);
+    }
+}
